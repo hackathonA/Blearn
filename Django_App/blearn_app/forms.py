f0d0c837a00eb405ed0f68bb8c8cd9d47ceccebf
harumakin <shirasaka0116@gmail.com>
2022-11-03 16:55:54 +0900

    カテゴリテーブルを追加

--- Django_App/blearn_app/forms.py
+++ Django_App/blearn_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Content
 
+
 class ContentForm(forms.ModelForm):
     class Meta:
         model = Content
--- Django_App/blearn_app/models.py
+++ Django_App/blearn_app/models.py
@@ -3,6 +3,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Contentに紐付けるカテゴリー
+class Category(models.Model):
+    name = models.CharField(
+        max_length=30,
+        verbose_name='カテゴリ名',
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return self.name
+
 class Content(models.Model):
     title = models.CharField(max_length=140)
     # blurがかかった単語
@@ -11,6 +23,7 @@ class Content(models.Model):
     word = models.CharField(max_length=140)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name=("カテゴリ名"), on_delete=models.CASCADE, default='0')
 
     def __str__(self):
         return self.title
